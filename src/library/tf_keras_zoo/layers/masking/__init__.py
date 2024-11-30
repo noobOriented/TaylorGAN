3d33fa4d8c30307b615be09f5ca66f8f9da55283
noobOriented <jsaon92@gmail.com>
2024-11-30 22:02:19 +0800

    remove tensorflow part

--- src/library/tf_keras_zoo/layers/masking/__init__.py
+++
@@ -1,6 +0,0 @@
-from .apply_mask import ApplyMask
-from .mask_conv import MaskConv1D
-from .pooling import (
-    MaskAveragePooling1D, MaskAvgPool1D, MaskGlobalAveragePooling1D, MaskGlobalAvgPool1D,
-    MaskGlobalMaxPool1D, MaskGlobalMaxPooling1D, MaskMaxPool1D, MaskMaxPooling1D,
-)
--- src/library/tf_keras_zoo/optimizers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .gradient_clipping import GradientClipping
-from .look_ahead import LookAhead
-from .radam import RAdamOptimizer
-from .weight_decay import WeightDecay
