a283e95871f23c79e1d6dd0b94d353afdea772db
noobOriented <jsaon92@gmail.com>
2024-12-07 21:38:45 +0800

    move files

--- src/core/preprocess/_config_objects.py
+++ src/core/preprocess/_config_objects.py
@@ -5,11 +5,39 @@ import typing as t
 import unicodedata
 
 import more_itertools
+import numpy as np
 import pydantic
+import umsgpack
 
 from library.utils import format_path, logging_indent, tqdm_open
 
-from ._adaptors import WordEmbeddingCollection
+
+class WordEmbeddingCollection:
+
+    UNK = '<unk>'
+
+    def __init__(self, token2index: t.Mapping[str, int], vectors: t.Sequence[t.Sequence[float]]):
+        self.token2index = token2index
+        self.vectors = np.asarray(vectors, np.float32)
+
+    @classmethod
+    def load_msg(cls, path: str | os.PathLike[str]):
+        with open(path, "rb") as f_in:
+            params = umsgpack.unpack(f_in)
+        return cls(token2index=params['token2index'], vectors=params['vector'])
+
+    def get_matrix_of_tokens(self, token_list: t.Sequence[str]) -> npt.NDArray[np.float32]:
+        return np.array([
+            self._get_vector_of_token(token)
+            for token in token_list
+        ])
+
+    def _get_vector_of_token(self, token: str):
+        if token in self.token2index:
+            return self.vectors[self.token2index[token]]
+        if self.UNK in self.token2index:
+            return self.vectors[self.token2index[self.UNK]]
+        return np.zeros_like(self.vectors[0])
 
 
 class LanguageConfig(pydantic.BaseModel):
--- src/core/preprocess/tests/test_preprocessors.py
+++ src/core/preprocess/tests/test_preprocessors.py
@@ -1,6 +1,70 @@
-from .. import Preprocessor
+import numpy as np
+import pytest
+
+from .. import CorpusConfig, LanguageConfig, Preprocessor, Tokenizer
+from .._config_objects import WordEmbeddingCollection
+
+
+@pytest.fixture(scope='session')
+def cache_root_dir(tmpdir_factory):
+    return tmpdir_factory.mktemp('cache')
+
+
+@pytest.fixture(scope='session', autouse=True)
+def redirect_cache_root(cache_root_dir):
+    from core.cache import cache_center
+    cache_center.root_path = str(cache_root_dir)
+
+
+@pytest.fixture(scope='session')
+def language_config(data_dir):
+    return LanguageConfig(
+        embedding_path=data_dir / 'en_fasttext_word2vec_V100D20.msg',
+        split_token=' ',
+    )
+
+
+@pytest.fixture(scope='session')
+def corpus_config(data_dir, language_config):
+    return CorpusConfig(
+        'test',
+        path=data_dir / 'train.txt',
+        maxlen=10,
+        language_config=language_config,
+    )
 
 
 def test_uttut_preprocessor(corpus_config):
     data_collection, _ = Preprocessor(corpus_config).preprocess()
     assert data_collection['train'].ids.shape[1] == corpus_config.maxlen
+
+
+class TestWordEmbeddingCollection:
+
+    def test_get_matrix(self):
+        wordvec = WordEmbeddingCollection(
+            {'a': 0, 'b': 1, 'c': 2, WordEmbeddingCollection.UNK: 3},
+            [[0, 1], [2, 3], [4, 5], [6, 7]],
+        )
+        assert np.array_equal(
+            wordvec.get_matrix_of_tokens(['b', 'd is unk', 'a']),
+            [[2, 3], [6, 7], [0, 1]],
+        )
+
+
+class TestTokenizer:
+
+    @pytest.fixture(scope='class')
+    def tokenizer(self, corpus_config):
+        return Tokenizer.fit_corpus(corpus_config)
+
+    def test_mapping_consistent(self, tokenizer, corpus_config):
+        with open(corpus_config.path['train'], 'r') as f:
+            line = f.readline()
+            ids1 = tokenizer.text_to_ids(line)
+            text1 = tokenizer.ids_to_text(ids1)
+            ids2 = tokenizer.text_to_ids(text1)
+            text2 = tokenizer.ids_to_text(ids2)
+
+        assert text1 == text2
+        assert ids1 == ids2
